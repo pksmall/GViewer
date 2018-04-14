fa5979a0f6b7074d367042d8c9d66adfdcc6e627
Pavel Korzhenko <pksmall@live.ru>
2018-04-14 15:41:43 +0300

    ловим ошибки и отписываемся от субскрайбера рх

--- app/src/main/java/office/small/gviewer/presenter/InfoPresenterImpl.java
+++ app/src/main/java/office/small/gviewer/presenter/InfoPresenterImpl.java
@@ -4,21 +4,32 @@ import com.hannesdorfmann.mosby3.mvp.MvpBasePresenter;
 
 import office.small.gviewer.model.InfoModel;
 import office.small.gviewer.view.InfoView;
+import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
 
 public class InfoPresenterImpl extends MvpBasePresenter<InfoView> implements InfoPresenter {
     private final InfoModel model;
     private InfoView infoView;
+    private Subscription subscription;
 
     public InfoPresenterImpl(InfoModel model) {
         this.model = model;
     }
 
     @Override
+    public void detachView(boolean retainInstance) {
+        if (!retainInstance && subscription != null && !subscription.isUnsubscribed()) {
+            subscription.unsubscribe();
+            subscription = null;
+        }
+        super.detachView(retainInstance);
+    }
+
+    @Override
     public void loadInformation(final boolean pullToRefresh) {
         infoView = getView();
-        model.retrieveInfo().observeOn(AndroidSchedulers.mainThread())
+        subscription = model.retrieveInfo().observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Action1<String>() {
                     @Override
                     public void call(String s) {
